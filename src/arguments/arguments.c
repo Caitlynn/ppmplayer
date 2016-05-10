cac16fb31bbd2d37bcff0693ee3bf8540cc6b735
Caitlynn <chow.tian.oo@gmail.com>
2016-05-11 00:00:32 +1000

    .

--- src/arguments/arguments.c
+++ src/arguments/arguments.c
@@ -8,33 +8,34 @@
 
 int validateArguments(Arguments *args, int argc, char **argv){
 	/*validate the number of the arguments*/
+	//@todo: argc should less than 7
 	if (argc < 1){
 		fprintf(stderr, "Please enter correct arguments!\n");
 		return false;
 	} 
 
-	args->delay = (unsigned int)argv[1];
+	args->delay = atoi(argv[1]);
+	//@todo: fix this as taking argument.
+	// const struct option long_options[] = {
+	// 	{ "brightness", 1, NULL, 'b'},
+	// 	{ "contrast", 1, NULL, 'c'},
+	// 	{ "saturation", 1, NULL, 's'}
+	// };
+	// int indexptr;
+	// int arg;
 
-	const struct option long_options[] = {
-		{ "brightness", 1, NULL, 'b'},
-		{ "contrast", 1, NULL, 'c'},
-		{ "saturation", 1, NULL, 's'}
-	};
-	int indexptr;
-	int arg;
-
-	while((arg = getopt_long(argc, argv, "b:c:s", long_options, &indexptr)) != -1){
-		switch(arg){
-			case 'b':
-				args->brightness = atoi(optarg);
-				break;
-			case 'c':
-				args->contrast = atoi(optarg);
-				break;
-			case 's':
-				args->saturation = atoi(optarg);
-				break;
-		}
-	}
-	return true;
+	// while((arg = getopt_long(argc, argv, "b:c:s", long_options, &indexptr)) != -1){
+	// 	switch(arg){
+	// 		case 'b':
+	// 			args->brightness = atoi(optarg);
+	// 			break;
+	// 		case 'c':
+	// 			args->contrast = atoi(optarg);
+	// 			break;
+	// 		case 's':
+	// 			args->saturation = atoi(optarg);
+	// 			break;
+	// 	}
+	// }
+	// return true;
 }
